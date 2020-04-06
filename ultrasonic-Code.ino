66ca8c92e09b4a0e2dd6c59c40c59bbef32b3e39
IoTLAB <46087841+IoTLAB-IQ@users.noreply.github.com>
2020-04-06 09:43:09 +0300

    Update ultrasonic-Code.ino

--- ultrasonic-Code.ino
+++ ultrasonic-Code.ino
@@ -1,11 +1,13 @@
-
-const int trigPin = 9;
-const int echoPin = 10;
+int trigPin = 9;
+int echoPin = 8;
+int LED =3;
 // defines variables
 long duration;
 int distance;
+
 void setup() {
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(LED, OUTPUT);
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 Serial.begin(9600); // Starts the serial communication
 }
@@ -17,6 +19,8 @@ delayMicroseconds(2);
 digitalWrite(trigPin, HIGH);
 delayMicroseconds(10);
 digitalWrite(trigPin, LOW);
+
+
 // Reads the echoPin, returns the sound wave travel time in microseconds
 duration = pulseIn(echoPin, HIGH);
 // Calculating the distance/ speed of sound, of course! Its 340 m/s. We have to convert the speed of sound into cm/µs 
@@ -24,4 +28,11 @@ distance= duration*0.034/2;
 // Prints the distance on the Serial Monitor
 Serial.print("Distance: ");
 Serial.println(distance);
+delay (500);
+if (distance <10 ){
+digitalWrite(LED, HIGH);
+}
+else
+digitalWrite(LED, LOW);
+
 }
